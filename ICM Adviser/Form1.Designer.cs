9667d151ad7f85d46555b12389e3632aac708c43
George <george.pliner@amdocs.com>
2011-09-06 12:34:01 +0300

    Add form elements

--- ICM Adviser/Form1.Designer.cs
+++ ICM Adviser/Form1.Designer.cs
@@ -28,12 +28,233 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelPL = new System.Windows.Forms.Label();
+            this.labelP = new System.Windows.Forms.Label();
+            this.labelM = new System.Windows.Forms.Label();
+            this.numericUpDownPL = new System.Windows.Forms.NumericUpDown();
+            this.radioButtonReise = new System.Windows.Forms.RadioButton();
+            this.radioButtonFold = new System.Windows.Forms.RadioButton();
+            this.radioButtonL = new System.Windows.Forms.RadioButton();
+            this.numericUpDownP = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownM = new System.Windows.Forms.NumericUpDown();
+            this.labelRange = new System.Windows.Forms.Label();
+            this.textBoxRange = new System.Windows.Forms.TextBox();
+            this.buttonShow = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPL)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownP)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownM)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelPL
+            // 
+            this.labelPL.AutoSize = true;
+            this.labelPL.Location = new System.Drawing.Point(35, 72);
+            this.labelPL.Name = "labelPL";
+            this.labelPL.Size = new System.Drawing.Size(20, 13);
+            this.labelPL.TabIndex = 0;
+            this.labelPL.Text = "PL";
+            // 
+            // labelP
+            // 
+            this.labelP.AutoSize = true;
+            this.labelP.Location = new System.Drawing.Point(118, 72);
+            this.labelP.Name = "labelP";
+            this.labelP.Size = new System.Drawing.Size(14, 13);
+            this.labelP.TabIndex = 1;
+            this.labelP.Text = "P";
+            // 
+            // labelM
+            // 
+            this.labelM.AutoSize = true;
+            this.labelM.Location = new System.Drawing.Point(206, 72);
+            this.labelM.Name = "labelM";
+            this.labelM.Size = new System.Drawing.Size(16, 13);
+            this.labelM.TabIndex = 2;
+            this.labelM.Text = "M";
+            // 
+            // numericUpDownPL
+            // 
+            this.numericUpDownPL.Location = new System.Drawing.Point(36, 109);
+            this.numericUpDownPL.Maximum = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            this.numericUpDownPL.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDownPL.Name = "numericUpDownPL";
+            this.numericUpDownPL.Size = new System.Drawing.Size(56, 20);
+            this.numericUpDownPL.TabIndex = 3;
+            this.numericUpDownPL.Value = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            // 
+            // radioButtonReise
+            // 
+            this.radioButtonReise.AutoSize = true;
+            this.radioButtonReise.Location = new System.Drawing.Point(205, 30);
+            this.radioButtonReise.Name = "radioButtonReise";
+            this.radioButtonReise.Size = new System.Drawing.Size(33, 17);
+            this.radioButtonReise.TabIndex = 4;
+            this.radioButtonReise.TabStop = true;
+            this.radioButtonReise.Text = "R";
+            this.radioButtonReise.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonFold
+            // 
+            this.radioButtonFold.AutoSize = true;
+            this.radioButtonFold.Location = new System.Drawing.Point(40, 30);
+            this.radioButtonFold.Name = "radioButtonFold";
+            this.radioButtonFold.Size = new System.Drawing.Size(31, 17);
+            this.radioButtonFold.TabIndex = 5;
+            this.radioButtonFold.TabStop = true;
+            this.radioButtonFold.Text = "F";
+            this.radioButtonFold.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonL
+            // 
+            this.radioButtonL.AutoSize = true;
+            this.radioButtonL.Location = new System.Drawing.Point(121, 30);
+            this.radioButtonL.Name = "radioButtonL";
+            this.radioButtonL.Size = new System.Drawing.Size(31, 17);
+            this.radioButtonL.TabIndex = 6;
+            this.radioButtonL.TabStop = true;
+            this.radioButtonL.Text = "L";
+            this.radioButtonL.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDownP
+            // 
+            this.numericUpDownP.Location = new System.Drawing.Point(108, 109);
+            this.numericUpDownP.Maximum = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.numericUpDownP.Name = "numericUpDownP";
+            this.numericUpDownP.Size = new System.Drawing.Size(55, 20);
+            this.numericUpDownP.TabIndex = 7;
+            this.numericUpDownP.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDownM
+            // 
+            this.numericUpDownM.Location = new System.Drawing.Point(205, 109);
+            this.numericUpDownM.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDownM.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownM.Name = "numericUpDownM";
+            this.numericUpDownM.Size = new System.Drawing.Size(29, 20);
+            this.numericUpDownM.TabIndex = 8;
+            this.numericUpDownM.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // labelRange
+            // 
+            this.labelRange.AutoSize = true;
+            this.labelRange.Location = new System.Drawing.Point(37, 152);
+            this.labelRange.Name = "labelRange";
+            this.labelRange.Size = new System.Drawing.Size(42, 13);
+            this.labelRange.TabIndex = 9;
+            this.labelRange.Text = "Range:";
+            // 
+            // textBoxRange
+            // 
+            this.textBoxRange.Location = new System.Drawing.Point(111, 149);
+            this.textBoxRange.Name = "textBoxRange";
+            this.textBoxRange.Size = new System.Drawing.Size(52, 20);
+            this.textBoxRange.TabIndex = 10;
+            // 
+            // buttonShow
+            // 
+            this.buttonShow.Location = new System.Drawing.Point(36, 186);
+            this.buttonShow.Name = "buttonShow";
+            this.buttonShow.Size = new System.Drawing.Size(56, 23);
+            this.buttonShow.TabIndex = 11;
+            this.buttonShow.Text = "Show";
+            this.buttonShow.UseVisualStyleBackColor = true;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(111, 184);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(55, 27);
+            this.buttonSave.TabIndex = 12;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(40, 224);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(198, 111);
+            this.textBox1.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 347);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonShow);
+            this.Controls.Add(this.textBoxRange);
+            this.Controls.Add(this.labelRange);
+            this.Controls.Add(this.numericUpDownM);
+            this.Controls.Add(this.numericUpDownP);
+            this.Controls.Add(this.radioButtonL);
+            this.Controls.Add(this.radioButtonFold);
+            this.Controls.Add(this.radioButtonReise);
+            this.Controls.Add(this.numericUpDownPL);
+            this.Controls.Add(this.labelM);
+            this.Controls.Add(this.labelP);
+            this.Controls.Add(this.labelPL);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPL)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownP)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownM)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelPL;
+        private System.Windows.Forms.Label labelP;
+        private System.Windows.Forms.Label labelM;
+        private System.Windows.Forms.NumericUpDown numericUpDownPL;
+        private System.Windows.Forms.RadioButton radioButtonReise;
+        private System.Windows.Forms.RadioButton radioButtonFold;
+        private System.Windows.Forms.RadioButton radioButtonL;
+        private System.Windows.Forms.NumericUpDown numericUpDownP;
+        private System.Windows.Forms.NumericUpDown numericUpDownM;
+        private System.Windows.Forms.Label labelRange;
+        private System.Windows.Forms.TextBox textBoxRange;
+        private System.Windows.Forms.Button buttonShow;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
