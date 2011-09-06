025408aa6fdf2be409c259453d09b5824e565e00
George <george.pliner@amdocs.com>
2011-09-06 13:26:41 +0300

    added  Save  menu

--- ICM Adviser/Form1.Designer.cs
+++ ICM Adviser/Form1.Designer.cs
@@ -42,9 +42,18 @@
             this.buttonShow = new System.Windows.Forms.Button();
             this.buttonSave = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPL)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownP)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownM)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // labelPL
@@ -212,6 +221,57 @@
             this.textBox1.Size = new System.Drawing.Size(198, 111);
             this.textBox1.TabIndex = 13;
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(273, 24);
+            this.menuStrip1.TabIndex = 14;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As..";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -231,11 +291,15 @@
             this.Controls.Add(this.labelM);
             this.Controls.Add(this.labelP);
             this.Controls.Add(this.labelPL);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPL)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownP)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownM)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -257,6 +321,14 @@
         private System.Windows.Forms.Button buttonShow;
         private System.Windows.Forms.Button buttonSave;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
 
--- ICM Adviser/Form1.cs
+++ ICM Adviser/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace ICM_Adviser
 {
@@ -14,6 +15,8 @@ namespace ICM_Adviser
         const int MAX_PL = 9;
         const int MAX_P  = 8;
         const int MAX_M  = 10;
+
+        private string m_filename = "C:\\XMLtest\\test.xml";
 
         private Decimal[, ,] Range = new Decimal[MAX_PL, MAX_P, MAX_M];
 
@@ -52,6 +55,59 @@ namespace ICM_Adviser
             decimal res = Convert.ToDecimal(this.textBoxRange.Text);
 
             Range[PL - 1, P - 1, M - 1] = res;
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult dlgValue = saveFileDialog1.ShowDialog();
+            if (dlgValue == DialogResult.OK)
+            {
+                // save
+                m_filename = saveFileDialog1.FileName;
+                saveXML();
+            }
+           
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveXML();
+        }
+
+        private  void saveXML()
+        {
+            XmlTextWriter myXmlTextWriter = null;
+
+            myXmlTextWriter = new XmlTextWriter(m_filename, null);
+            myXmlTextWriter.Formatting = Formatting.Indented;
+
+            for (int pl = 0; pl < MAX_PL; pl++)
+            {
+                myXmlTextWriter.WriteStartElement("PL");
+                myXmlTextWriter.WriteAttributeString("Value", Convert.ToString(pl));
+                for (int p = 0; p < MAX_P; p++)
+                {
+                    myXmlTextWriter.WriteStartElement("P");
+                    myXmlTextWriter.WriteAttributeString("Value", Convert.ToString(p));
+                    for (int m = 0; m < MAX_M; m++)
+                    {
+                        myXmlTextWriter.WriteStartElement("M");
+                        myXmlTextWriter.WriteAttributeString("Value", Convert.ToString(m));
+
+                        myXmlTextWriter.WriteElementString("Range", Range[pl, p, m].ToString());
+
+                        myXmlTextWriter.WriteEndElement();
+                    }
+                    myXmlTextWriter.WriteEndElement();
+                }
+                myXmlTextWriter.WriteEndElement();
+            }
+
+            //Write the XML to file and close the writer
+            myXmlTextWriter.Flush();
+            myXmlTextWriter.Close();
+            if (myXmlTextWriter != null)
+                myXmlTextWriter.Close();
         }
     }
 }
